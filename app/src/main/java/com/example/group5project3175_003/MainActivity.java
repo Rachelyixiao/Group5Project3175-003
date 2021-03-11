4e83085367a1e68f11504f88a0f71565b62ed99a
Rachelyixiao <angelinexiaoxiao@gmail.com>
2021-03-11 09:59:05 -0800

    some changes of bigexpense pages

--- app/src/main/java/com/example/group5project3175_003/MainActivity.java
+++ app/src/main/java/com/example/group5project3175_003/MainActivity.java
@@ -2,7 +2,10 @@ package com.example.group5project3175_003;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button btnTest = findViewById(R.id.btnTest);
+
+        btnTest.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, bigExpensePage1.class));
+            }
+        });
     }
 }
