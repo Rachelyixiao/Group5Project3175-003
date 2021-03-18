b1becacc1700e2aee2d0b2d2b428eed742fda194
Rachelyixiao <angelinexiaoxiao@gmail.com>
2021-03-17 23:41:52 -0700

    Add new pages of settings and link privacy policy website.

---
+++ app/src/main/java/com/example/group5project3175_003/CustomizeReport.java
@@ -0,0 +1,14 @@
+package com.example.group5project3175_003;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class CustomizeReport extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_customize_report);
+    }
+}
--- app/src/main/java/com/example/group5project3175_003/SettingPage1.java
+++ app/src/main/java/com/example/group5project3175_003/SettingPage1.java
@@ -3,8 +3,10 @@ package com.example.group5project3175_003;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ImageButton;
 import android.widget.ListView;
 import android.widget.SimpleAdapter;
@@ -14,7 +16,7 @@ import java.util.HashMap;
 import java.util.List;
 
 public class SettingPage1 extends AppCompatActivity {
-    String[] settings = {"SET PROFILE","CHOOSE THEME","HELP","PRIVACY POLICY","LOG OUT","BACK UP"};
+    String[] settings = {"Check/Modify User Info","Check User Account Info","Report History Research","PRIVACY POLICY","LOG OUT"};
     int[] settingImages = {R.drawable.settingprofile,R.drawable.settingtheme,R.drawable.settinghelp,
             R.drawable.settingprivacy,R.drawable.settinglogout,R.drawable.settingbackup};
 
@@ -25,7 +27,7 @@ public class SettingPage1 extends AppCompatActivity {
 
         List<HashMap<String,String>> aListSetting = new ArrayList<>();
 
-        for(int i =0;i<6;i++){
+        for(int i =0;i<5;i++){
             HashMap<String,String> hashMapSetting = new HashMap<>();
             hashMapSetting.put("txt",settings[i]);
             hashMapSetting.put("images",Integer.toString(settingImages[i]));
@@ -42,8 +44,31 @@ public class SettingPage1 extends AppCompatActivity {
 
         listViewS.setAdapter(adapterS);
 
-        ImageButton btnHome = findViewById(R.id.imgbtnHome);
+        listViewS.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                switch (position){
+                    case 0:
+                        startActivity(new Intent(SettingPage1.this,SettingPage2.class));
+                        break;
+                    case 1:
+                        startActivity(new Intent(SettingPage1.this,CheckAccount.class));
+                        break;
+                    case 2:
+                        startActivity(new Intent(SettingPage1.this,CustomizeReport.class));
+                        break;
+                    case 3:
+                        startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse("https://sites.google.com/view/moneyandmanagement3175003")));
+                        break;
+                    case 4:
+                        startActivity(new Intent(SettingPage1.this,MainActivity.class));
+                }
+            }
+        });
 
+
+
+        ImageButton btnHome = findViewById(R.id.imgbtnHome);
         btnHome.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
