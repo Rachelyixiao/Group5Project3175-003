86c68888831a5e2f9c23ff02d48115b759799a03
Rachelyixiao <angelinexiaoxiao@gmail.com>
2021-03-28 10:44:55 -0700

    change date type to date, use Calendar and Datepicker class

--- app/src/main/java/com/example/group5project3175_003/reportPage4.java
+++ app/src/main/java/com/example/group5project3175_003/reportPage4.java
@@ -1,29 +1,92 @@
 package com.example.group5project3175_003;
 
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.DatePickerDialog;
+import android.app.TimePickerDialog;
 import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
+import android.text.InputType;
 import android.view.View;
+import android.widget.Button;
+import android.widget.DatePicker;
+import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.TimePicker;
+
+import java.util.Calendar;
 
 public class reportPage4 extends AppCompatActivity {
+    DatePickerDialog datePickerDialog;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_report_page4);
 
+        EditText reportStart = findViewById(R.id.txtSDate);
+        EditText reportEnd = findViewById(R.id.txtEdate);
+
+        Button btnR4 = findViewById(R.id.btnR4submit);
+        TextView outputR4 = findViewById(R.id.textOutputR4);
+
+        reportStart.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                final Calendar c1 = Calendar.getInstance();
+                int reportSYear = c1.get(Calendar.YEAR);
+                int reportSMonth = c1.get(Calendar.MONTH);
+                int reportSDay = c1.get(Calendar.DAY_OF_MONTH);
+
+                datePickerDialog = new DatePickerDialog(reportPage4.this,
+                        new DatePickerDialog.OnDateSetListener() {
+                            @Override
+                            public void onDateSet(DatePicker view, int year,
+                                                  int monthOfYear, int dayOfMonth) {
+                                // set day of month , month and year value in the edit text
+                                reportStart.setText(dayOfMonth + "/"
+                                        + (monthOfYear + 1) + "/" + year);
+                            }
+                        }, reportSYear, reportSMonth, reportSDay);
+                datePickerDialog.show();
+            }
+        });
+
+        reportEnd.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                final Calendar c2 = Calendar.getInstance();
+                int reportEYear = c2.get(Calendar.YEAR);
+                int reportEMonth = c2.get(Calendar.MONTH);
+                int reportEDay = c2.get(Calendar.DAY_OF_MONTH);
+
+                datePickerDialog = new DatePickerDialog(reportPage4.this,
+                        new DatePickerDialog.OnDateSetListener() {
+                            @Override
+                            public void onDateSet(DatePicker view, int year,
+                                                  int monthOfYear, int dayOfMonth) {
+                                // set day of month , month and year value in the edit text
+                                reportEnd.setText(dayOfMonth + "/"
+                                        + (monthOfYear + 1) + "/" + year);
+                            }
+                        }, reportEYear, reportEMonth, reportEDay);
+                datePickerDialog.show();
+            }
+        });
 
 
 
-
-
-
-
-
-
-
+        btnR4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String startDate = reportStart.getText().toString();
+                String endDate = reportEnd.getText().toString();
+                outputR4.setText("Your report date is "+startDate+" "+endDate);
+            }
+        });
 
 
 
@@ -62,4 +125,7 @@ public class reportPage4 extends AppCompatActivity {
 
 
     }
+
+
+
 }
