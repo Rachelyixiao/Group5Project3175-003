c55d70d10fdd47766301ca0ddb63eb096db8655a
Rachelyixiao <angelinexiaoxiao@gmail.com>
2021-03-30 00:57:48 -0700

    Done the pie chart part.

--- app/src/main/java/com/example/group5project3175_003/transactionR.java
+++ app/src/main/java/com/example/group5project3175_003/transactionR.java
@@ -6,6 +6,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 
 public class transactionR extends AppCompatActivity {
 
@@ -30,6 +31,44 @@ public class transactionR extends AppCompatActivity {
                 startActivity(new Intent(transactionR.this, DailyExpense.class));
             }
         });
+
+
+
+        ImageView btnTracker_R = findViewById(R.id.btnTrackerR);
+        ImageView btnbig_R = findViewById(R.id.btnBigR);
+        ImageView btnReport_R = findViewById(R.id.btnReportR);
+        ImageView btnSetting_R = findViewById(R.id.btnSettingsR);
+
+
+
+        btnTracker_R.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(transactionR.this, MainActivity.class));
+            }
+        });
+
+        btnbig_R.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(transactionR.this, big1.class));
+            }
+        });
+
+        btnReport_R.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(transactionR.this, reportMain.class));
+            }
+        });
+
+        btnSetting_R.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(transactionR.this, SettingPage1.class));
+            }
+        });
+
 
 
     }
